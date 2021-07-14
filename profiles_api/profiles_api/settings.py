5d94574f28cb825effdaf1b50711f45bb696415f
Reneta <reneta.dimitrova96@gmail.com>
2021-07-14 15:05:59 +0300

    Added custom user profile model, manager and migrations

--- profiles_api/profiles_api/settings.py
+++ profiles_api/profiles_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'profiles_project',
+
 ]
 
 MIDDLEWARE = [
@@ -118,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_project.UserProfile'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
